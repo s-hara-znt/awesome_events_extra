5490eca5bb22121b550b6c83422e8072e6f05073
s-hara <s-hara_mc@zenet-web.co.jp>
2021-07-27 11:58:01 +0900

    seed file add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Event.destroy_all
+Ticket.destroy_all
+
+10.times do |user_num|
+  User.create(uid: "#{user_num}", provider: "github", name: "name #{user_num}", image_url: "https://avatars.githubusercontent.com/u/87514709?v=4")
+  5.times do |event_num|
+    Event.create(owner_id: User.find_by(uid: user_num).id, name: "event # #{event_num}", place: "place #{event_num}", start_at: Time.new + (60 * 60), end_at: Time.new + (60 * 60 * 2), content: "content #{event_num}")
+  end
+end
+
+Event.all.ids.sort.each do |event_id|
+  User.all.ids.sort.each do |user_id|
+    Ticket.create(user_id: user_id, event_id: event_id, comment: "User #{user_id} sample comment")
+  end
+end
